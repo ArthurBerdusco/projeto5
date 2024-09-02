fe9a86047906143f10507d001d5151cb9b8a4cfa
Letícia <iambragalets@gmail.com>
2024-09-01 21:34:25 -0300

    navegacao depois do login funcionando

--- frontend/app/screen/responsavel/index.tsx
+++ frontend/app/screen/responsavel/index.tsx
@@ -1,10 +1,41 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { ScrollView, View, StyleSheet, Text } from "react-native";
 
 export default function Index() {
     return (
-        <View>
-            <Text>Olá responsavel</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={styles.total}>
+
+                <View>
+                    <Text style={{ fontSize: 20, fontWeight: "500" }}>Meus dados</Text>
+                    <View style={styles.cardDados}>
+
+                    </View>
+                </View>
+
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        backgroundColor: "#ffffff",
+    },
+
+    total: {
+        display: "flex",
+        alignItems: "center",
+        margin: 20
+    },
+
+    cardDados: {
+        backgroundColor: "#464646",
+        width: 400,
+        height: 150,
+        borderRadius: 20
+    }
+
+
+})
